95cf1e7fc3d6fda506cf593c258a7d835f0a059d
Akash Bhaskar <i.am.akashbhaskar@accubits.com>
2023-03-27 03:23:35 +0530

    shorten url api

--- src/modules/shortUrl.js
+++ src/modules/shortUrl.js
@@ -1,1 +1,18 @@
-const {messages}=require
+const { messages } = require('../config');
+const { response, stringGenerator } = require('../utils');
+const URL = require('../models/url');
+
+const shortUrl = async (req, res) => {
+    try {
+        const { body } = req;
+        const short = await stringGenerator();
+        await URL.create({
+            url: body.url,
+            shortUrl: short,
+        });
+        return response.success(res, messages.urlShortened, short);
+    } catch (error) {
+        return response.serverError(res, error);
+    }
+};
+module.exports = shortUrl;
